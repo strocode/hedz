8415e16886a502b0d02b0d5484869ae959c149ea
Bannister, Keith (CASS, Marsfield) <Keith.Bannister@csiro.au>
2020-05-14 06:57:43 +1000

    Fiddle around with locations of everything - start game on newgame. Javascript paths fail but getting there

--- spacehedz/server/hedzapp/routes/index.js
+++ spacehedz/server/hedzapp/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const crypto = require('crypto');
 var router = express.Router();
+const path = require('path');
+const jsdom = require('jsdom');
+const Datauri = require('datauri');
+const datauri = new Datauri();
+const { JSDOM } = jsdom;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +14,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* Go to new game. Rediects to a random game page */
-router.get('/newgame', function(req, res, next) {
+router.get('/newgame', async function(req, res, next) {
   // Make a number
   let randnum = Math.round(Math.random()*1e9);
   //let randstr = randnum.toString(36);
@@ -18,12 +24,14 @@ router.get('/newgame', function(req, res, next) {
   crypto.randomFillSync(randarr);
 
   // take those numbers and turn them into a base36 string array.
-  let randstr = randarr.reduce((acc, val) => acc + val.toString(36), '');
-  console.log('New game randstr' + randstr);
-  res.cookie("gameid", randstr, {
-    maxAge: 1000*60*60*24*30, // 30 days in milliseconds
+  let gameId = randarr.reduce((acc, val) => acc + val.toString(36), '');
+  console.log('New game randstr' + gameId);
+  res.cookie("gameid", gameId, {
+    maxAge: 1000*60*60, // milliseconds
   });
-  res.redirect(`/game/${randstr}`);
+  const io = req.app.get('socketio');
+  let dom = await setupAuthoritativePhaser(gameId, io);
+  res.redirect(`/game/${gameId}`);
 });
 
 router.get('/game/:gameId', function(req, res, next) {
@@ -31,7 +39,35 @@ router.get('/game/:gameId', function(req, res, next) {
     console.log('GameID is ' + gameId);
 
     // send game HTml file to client
-    res.send(gameId);
+    res.sendFile(path.join(__dirname, '../public/rockethedz.html'));
 });
+
+function setupAuthoritativePhaser(gameId, io) {
+  return new Promise((resolve, reject) => {
+    JSDOM.fromFile(path.join(__dirname, '../rockethedz_game_server/index.html'), {
+      // To run the scripts in the html file
+      runScripts: "dangerously",
+      // Also load supported external resources
+      resources: "usable",
+      // So requestAnimatinFrame events fire
+      pretendToBeVisual: true
+    }).then((dom) => {
+
+      // Add createObjectURL and revokeObjectURL because JSDOM doesn't have implementations
+      dom.window.URL.createObjectURL = (blob) => {
+        if (blob){
+          return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+        }
+      };
+      dom.window.URL.revokeObjectURL = (objectURL) => {};
+      dom.window.gameLoaded = () => {
+        resolve(dom);
+      };
+      dom.window.io = io;
+    }).catch((error) => {
+      reject(Error(error.message));
+    });
+  });
+}
 
 module.exports = router;
